1012cdd016bdfa21e37d40661bb385c670db2fd0
tugasSBD <ian25yola@gmail.com>
2021-08-27 19:01:03 +0700

    perbaikan deploy ke hosting

--- allPortofolio.php
+++ allPortofolio.php
@@ -1,9 +1,9 @@
 <?php
-    include('tambahportofolio/common/config.php');
+    include('./tambahportofolio/common/config.php');
     $sub = mysqli_query($con,"SELECT * FROM projects ORDER BY project_date DESC");
     while($row=mysqli_fetch_array($sub)){
         ?>
-        <div class="col-sm-6 col-md-6 col-lg-4 h-100 myporto mt-5">
+        <div class="col-sm-6 col-md-6 col-lg-4 h-100 myporto mt-5" data-aos="zoom-out" data-aos-easing="linear">
             <a class="project-links" href="portofolioDetail.html?kode=<?php echo htmlentities($row['id_project']) ?>" style="text-decoration:none">
                 <div class="project">
                     <div class="image-place">
@@ -15,7 +15,7 @@
                         $num = mysqli_num_rows($tags);
                         $i=0;
                     ?>
-                    <p class="text-center">Tags : <?php while($tag = mysqli_fetch_array($tags)){ 
+                    <p class="text-center text-dark">Tags : <?php while($tag = mysqli_fetch_array($tags)){ 
                             $i++;
                             if($i != $num){
                                 echo htmlentities($tag['tag_name']);
--- portofolioDetail.php
+++ portofolioDetail.php
@@ -1,5 +1,5 @@
 <?php
-    include('tambahPortofolio/common/config.php');
+    include('./tambahPortofolio/common/config.php');
     if(!empty($_POST['id'])){
         $id = $_POST['id'];
         $getDetail = mysqli_query($con, "SELECT * FROM projects WHERE id_project='".$id."'");
@@ -18,7 +18,22 @@
                     </div>
                     <div class="col-md-3 text">
                         <?php echo htmlentities($row['project_explanation']) ?>
-                        <p class="tags">Tags : HTML, CSS</p>
+                        <?php 
+                        $tags = mysqli_query($con, "SELECT * FROM projecttags INNER JOIN tags ON projecttags.id_tag=tags.id_tag WHERE projecttags.id_project='".$row['id_project']."'");
+                        $num = mysqli_num_rows($tags);
+                        $i=0;
+                        ?>
+                        <p class="tags">Tags : <?php while($tag = mysqli_fetch_array($tags)){ 
+                                $i++;
+                                if($i != $num){
+                                    echo htmlentities($tag['tag_name']);
+                                    echo ", ";    
+                                }else{
+                                    echo htmlentities($tag['tag_name']);
+                                }
+                            } 
+                        ?></p>
+                        <!-- <p class="tags">Tags : HTML, CSS</p> -->
                         <div class="buttons">
                             <div class="button1 text-center"><a href="<?php echo htmlentities($row['project_url']) ?>" target="_blank">Live Website</a></div>
                             <div class="button2 text-center"><a href="<?php echo htmlentities($row['project_github']) ?>"target="_blank">Source</a></div>
